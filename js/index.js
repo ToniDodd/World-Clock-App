ebc93645749bf911cf70cc73f24c12356bca2cfe
ToniDodd <121724318+ToniDodd@users.noreply.github.com>
2023-02-08 18:53:18 -0800

    Added link back to world clock

--- js/index.js
+++ js/index.js
@@ -41,7 +41,8 @@ function updateCity(event){
     <div class="date">${cityTime.format("MMMM Do, YYYY")}</div>
  </div>
     <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small></div>
- </div>`;
+ </div>
+ <a href="/">Back to cities list</a>`;
 }
 
 let citiesSelect = document.querySelector("#city");
